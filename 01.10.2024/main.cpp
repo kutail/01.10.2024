ac796f168badf1ef8d15db1e469a02d921cfe030
kutail <kutep0v.vlad.m@gmail.com>
2024-10-01 14:51:46 +0500

    game

--- 01.10.2024/main.cpp
+++ 01.10.2024/main.cpp
@@ -1,14 +1,42 @@
-//
-//  main.cpp
-//  01.10.2024
-//
-//  Created by Владислав on 01.10.2024.
-//
-
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    const int size = 2;
+    int a = rand() % 1 + 5;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = a;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Введите любое лисло от 0 до 10\n";
+        int chislo = 0;
+        cin >> chislo;
+        if (chislo < 0 || chislo > 10)
+        {
+            cout << "Число должно быть больше нуля и меньше десяти";
+        }
+        else
+        {
+            if (arr[i] == chislo)
+            {
+                
+                cout << "Поздравляю, число угадано\n";
+            }
+            else
+            {
+                
+                cout << "НЕудача\n";
+            }
+        }
+    }
+    
+    
     return 0;
 }
